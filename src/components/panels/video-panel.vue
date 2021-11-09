7fd5ca22a6d8d798735cff1924e6ab4b036169d1
Ryan Coulson <contact@ryan-coulson.com>
2021-11-09 08:48:03 -0500

    make slides use actual config; create temporary components

---
+++ src/components/panels/video-panel.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="bg-gray-100 text-center inline-block h-full align-middle">
+        <h1 class="align-middle">Temporary Video Panel</h1>
+    </div>
+</template>
+
+<script lang="ts">
+import { TextPanel } from '@/definitions';
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+@Component({})
+export default class VideoPanelV extends Vue {
+    @Prop() config!: TextPanel;
+}
+</script>
+
+<style lang="scss"></style>
