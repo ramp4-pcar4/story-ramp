16eb00961d21c15fd7aec4ced458ec6dc634fe3d
Daniel Nashed <dnlnashed@gmail.com>
2023-08-22 15:43:16 -0400

    replace vue-loading-spinner package

--- src/components/panels/loading-panel.vue
+++ src/components/panels/loading-panel.vue
@@ -1,7 +1,7 @@
 <template>
     <VueScrollama class="flex-1 prose max-w-none my-5">
         <div class="block py-20 align-middle text-center h-full" style="margin: 0 auto">
-            <!-- <spinner size="120px" background="#00D2D3" color="#009cd1" stroke="10px" style="margin: 0 auto"></spinner> -->
+            <spinner size="120px" color="#009cd1" style="margin: 0 auto"></spinner>
         </div>
     </VueScrollama>
 </template>
@@ -9,12 +9,12 @@
 <script lang="ts">
 import VueScrollama from 'vue3-scrollama';
 import { Options, Vue } from 'vue-property-decorator';
-// import Circle2 from 'vue-loading-spinner/src/components/Circle2.vue';
+import { VueSpinnerOval } from 'vue3-spinners';
 
 @Options({
     components: {
-        VueScrollama
-        // spinner: Circle2
+        VueScrollama,
+        spinner: VueSpinnerOval
     }
 })
 export default class LoadingPanelV extends Vue {}
