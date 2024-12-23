e4f7110c3fb73f218d0fe82b8aa3218973903f44
Miles Petrov <miles.petrov@ec.gc.ca>
2024-12-23 11:20:30 -0500

    Update build config, bump up ramp to v4.10.0

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import router from './router';
 import './style.css';
 import { i18n } from './lang';
 
-import 'ramp-pcar/dist/lib/ramp.css';
+import 'ramp-pcar/dist/bundler/ramp.css';
 import VueTippy from 'vue-tippy';
 import HighchartsVue from 'highcharts-vue';
 import VuePapaParse from 'vue-papa-parse';
