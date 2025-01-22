5ad6b80e162a5d9e32d7d6176867bda0a1c15913
yileifeng <31557789+yileifeng@users.noreply.github.com>
2025-01-22 21:27:28 +0000

    Deploying to gh-pages from @ ramp4-pcar4/story-ramp@eb12833cc5091f0c30ca8d4f98e3d4d40f57d6bb 🚀

--- main/assets/executeForIds-Z1gkLduF-D7pUGj-h.js
+++
@@ -1,1 +0,0 @@
-import{y as c}from"./utils-9sQxfkoa-PIByIh1h.js";import{P as m,N as f}from"./query-DFW9-NG_-CMGjG_sF.js";import{b as i}from"./Query-BrwMGK8U-Emn-7FKl.js";async function p(t,o,r){const n=c(t),{data:a}=await m(n,i.from(o),r);return a.count}async function d(t,o,r){const n=c(t),{data:a}=await f(n,i.from(o),r);return a.objectIds}export{d as e,p as i};
