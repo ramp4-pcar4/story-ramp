a69e7df7503b142231d8e347f3367601bf952b37
Yi Lei Feng <fengyilei1@gmail.com>
2025-01-28 18:58:51 +0000

    Delete PR demo from GH pages refix-bg-bug

--- refix-bg-bug/assets/calcite-icon-laqhdRBY-Cukoilvb.js
+++
@@ -1,1 +0,0 @@
-import{f as t,m as o}from"./icon-BXFbBbop-grEIEjLR.js";import"./themeUtils-YjM7iIiX-IRHTistd.js";import"./story-DleBxxVb.js";import"./main-DiASSDZe.js";import"./uuid-Dj9mdEVg-BaKSCiyT.js";import"./dom-N58V0tYi-DBU3jlah.js";import"./observers-BigwqTsw-CXamkfJr.js";const a=t,c=o;export{a as CalciteIcon,c as defineCustomElement};
