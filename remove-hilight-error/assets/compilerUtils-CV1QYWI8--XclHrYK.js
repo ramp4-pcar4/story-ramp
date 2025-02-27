e1f68f2a9b5da4b19f7889b020355989b88e42ad
yileifeng <yileifeng@users.noreply.github.com>
2025-02-27 15:54:48 +0000

    Deploying to gh-pages from @ ramp4-pcar4/story-ramp@f7f0ed61f5abbe4e397b3218d06bbc3c652437ca 🚀

---
+++ remove-hilight-error/assets/compilerUtils-CV1QYWI8--XclHrYK.js
@@ -0,0 +1,1 @@
+import"./story-CeSwA6O5.js";function o(n){}function r(n){return()=>n}export{r as o,o as t};
---
+++ remove-hilight-error/assets/screen-D-fcMt_1-D6FGPbob.js
@@ -0,0 +1,221 @@
+import{eT as E,fW as A,eX as R,eY as V,f6 as T,eZ as _,fY as C,f8 as j,eV as X,f7 as q,f9 as F,e_ as U,e$ as n,f0 as G,f1 as k,fG as B,fg as c,f4 as p,fb as d,f2 as h}from"./story-CeSwA6O5.js";import{u as D}from"./marked.esm-DgqJBp_S-CCvGNfAT.js";import"./main-B2WqJNyC.js";const Y=`<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet version="1.0"
+                xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+                xmlns:gmd="http://www.isotc211.org/2005/gmd"
+                xmlns:gco="http://www.isotc211.org/2005/gco"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                xmlns:gmdl="http://www.canada.gc.ca/ns/gmdl"
+                xmlns:napec="http://www.ec.gc.ca/data_donnees/standards/schemas/napec"
+                xmlns:gml="http://www.opengis.net/gml"
+                xmlns:geonet="http://www.fao.org/geonetwork"
+                xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                xsi:schemaLocation="http://www.ec.gc.ca/data_donnees/standards/schemas/napec/schema.xsd">
+
+  <xsl:param name="catalogue_url" />
+  <xsl:decimal-format NaN=""/>
+
+  <xsl:template match="/">
+
+    <div class="metadata-view">
+
+      <xsl:if test="//gmd:abstract/gco:CharacterString/text() != ''">
+        <h5 class="text-xl font-bold mb-3">{{metadata.xslt.Abstract}}</h5>
+        <p>
+          <xsl:value-of select="//gmd:abstract/gco:CharacterString/text()" />
+        </p>
+      </xsl:if>
+
+      <xsl:comment>
+        <h5 class="text-xl font-bold mb-3">{{metadata.xslt.Scope}}</h5>
+        <p>{{metadata.xslt.hereBeScope}}</p>
+      </xsl:comment>
+
+      <xsl:if test="//gml:TimePeriod//* != ''">
+        <h5 class="text-xl font-bold mb-3">{{metadata.xslt.timePeriod}}</h5>
+        <p>
+          <xsl:value-of select="//gml:TimePeriod//gml:beginPosition" />
+          <xsl:if test="//gml:TimePeriod//gml:beginPosition/text() != '' and //gml:TimePeriod//gml:endPosition/text() != ''">
+            -
+          </xsl:if>
+          <xsl:value-of select="//gml:TimePeriod//gml:endPosition" />
+        </p>
+      </xsl:if>
+
+      <xsl:comment>
+        <xsl:if test="//gmd:supplementalInformation/gco:CharacterString/text() != ''">
+          <h5 class="text-xl font-bold mb-3">{{metadata.xslt.supplementalData}}</h5>
+          <p>
+            <xsl:value-of select="//gmd:supplementalInformation/gco:CharacterString/text()" />
+          </p>
+        </xsl:if>
+      </xsl:comment>
+
+      <xsl:if test="//gmd:pointOfContact//gmd:individualName/* != '' 
+              or //gmd:pointOfContact//gmd:organisationName/gco:CharacterString/text() != ''
+              or //gmd:pointOfContact//gmd:positionName/gco:CharacterString/text() != ''
+              or //gmd:pointOfContact//gmd:electronicMailAddress/* != ''
+              or //gmd:pointOfContact//gmd:role/gmd:CI_RoleCode/@codeListValue != ''">
+        <h5 class="text-xl font-bold mb-3">{{metadata.xslt.contactInfo}}</h5>
+        <p>
+          <xsl:value-of select="//gmd:pointOfContact//gmd:individualName" />
+        </p>
+        <p>
+          <xsl:value-of select="//gmd:pointOfContact//gmd:organisationName/gco:CharacterString/text()" />
+        </p>
+        <p>
+          <xsl:value-of select="//gmd:pointOfContact//gmd:positionName/gco:CharacterString/text()" />
+        </p>
+        <p>
+          <a href="mailto:{//gmd:pointOfContact//gmd:electronicMailAddress/gco:CharacterString/text()}?Subject={//gmd:identificationInfo//gmd:title/gco:CharacterString/text()}">
+            <xsl:value-of select="//gmd:pointOfContact//gmd:electronicMailAddress" />
+          </a>
+        </p>
+        <p>
+          <xsl:variable name="roleCode" >
+            <xsl:value-of select="concat(substring(//gmd:pointOfContact//gmd:role/gmd:CI_RoleCode/@codeListValue,1,1),
+                        substring(//gmd:pointOfContact//gmd:role/gmd:CI_RoleCode/@codeListValue, 2))" />
+          </xsl:variable>
+
+          <xsl:choose>
+            <xsl:when test="$roleCode = 'resourceProvider'">{{metadata.xslt.resourceProvider}}</xsl:when>
+            <xsl:when test="$roleCode = 'custodian'">{{metadata.xslt.custodian}}</xsl:when>
+            <xsl:when test="$roleCode = 'owner'">{{metadata.xslt.owner}}</xsl:when>
+            <xsl:when test="$roleCode = 'user'">{{metadata.xslt.user}}</xsl:when>
+            <xsl:when test="$roleCode = 'distributor'">{{metadata.xslt.distributor}}</xsl:when>
+            <xsl:when test="$roleCode = 'originator'">{{metadata.xslt.originator}}</xsl:when>
+            <xsl:when test="$roleCode = 'pointOfContact'">{{metadata.xslt.pointOfContact}}</xsl:when>
+            <xsl:when test="$roleCode = 'principalInvestigator'">{{metadata.xslt.principalInvestigator}}</xsl:when>
+            <xsl:when test="$roleCode = 'processor'">{{metadata.xslt.processor}}</xsl:when>
+            <xsl:when test="$roleCode = 'publisher'">{{metadata.xslt.publisher}}</xsl:when>
+            <xsl:when test="$roleCode = 'author'">{{metadata.xslt.author}}</xsl:when>
+            <xsl:when test="$roleCode = 'collaborator'">{{metadata.xslt.collaborator}}</xsl:when>
+            <xsl:when test="$roleCode = 'editor'">{{metadata.xslt.editor}}</xsl:when>
+            <xsl:when test="$roleCode = 'mediator'">{{metadata.xslt.mediator}}</xsl:when>
+            <xsl:when test="$roleCode = 'rightsHolder'">{{metadata.xslt.rightsHolder}}</xsl:when>
+          </xsl:choose>
+        </p>
+      </xsl:if>
+
+      <xsl:if test="$catalogue_url != ''">
+        <h5 class="text-xl font-bold mb-3">{{metadata.xslt.cataloguePage}}</h5>
+        <p>
+          <a href="{$catalogue_url}"
+             rel="external" target="_blank" class="ui-link">
+            {{metadata.xslt.metadata}}
+          </a>
+        </p>
+      </xsl:if>
+    </div>
+  </xsl:template>
+</xsl:stylesheet>
+`,z=`<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet version="1.0"
+                xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+                xmlns:gmd="http://www.isotc211.org/2005/gmd"
+                xmlns:gco="http://www.isotc211.org/2005/gco"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                xmlns:gmdl="http://www.canada.gc.ca/ns/gmdl"
+                xmlns:napec="http://www.ec.gc.ca/data_donnees/standards/schemas/napec"
+                xmlns:gml="http://www.opengis.net/gml"
+                xmlns:geonet="http://www.fao.org/geonetwork"
+                xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                xsi:schemaLocation="http://www.ec.gc.ca/data_donnees/standards/schemas/napec/schema.xsd">
+
+  <xsl:param name="catalogue_url" />
+  <xsl:decimal-format NaN=""/>
+
+  <xsl:template match="/">
+
+    <div class="metadata-view">
+
+      <xsl:if test="//gmd:abstract//gmd:LocalisedCharacterString[@locale='#fra']/text() != ''">
+        <h5 class="text-xl font-bold mb-3">{{metadata.xslt.Abstract}}</h5>
+        <p>
+          <xsl:value-of select="//gmd:abstract//gmd:LocalisedCharacterString[@locale='#fra']/text()" />
+        </p>
+      </xsl:if>
+
+      <xsl:comment>
+        <h5 class="text-xl font-bold mb-3">{{metadata.xslt.Scope}}</h5>
+        <p>{{metadata.xslt.hereBeScope}}</p>
+      </xsl:comment>
+
+      <xsl:if test="//gml:TimePeriod//* != ''">
+        <h5 class="text-xl font-bold mb-3">{{metadata.xslt.timePeriod}}</h5>
+        <p>
+          <xsl:value-of select="//gml:TimePeriod//gml:beginPosition" />
+          <xsl:if test="//gml:TimePeriod//gml:beginPosition/text() != '' and //gml:TimePeriod//gml:endPosition/text() != ''">
+            -
+          </xsl:if>
+          <xsl:value-of select="//gml:TimePeriod//gml:endPosition" />
+        </p>
+      </xsl:if>
+
+      <xsl:comment>
+        <xsl:if test="//gmd:supplementalInformation//gmd:LocalisedCharacterString[@locale='#fra']/text() != ''">
+          <h5 class="text-xl font-bold mb-3">{{metadata.xslt.supplementalData}}</h5>
+          <p>
+            <xsl:value-of select="//gmd:supplementalInformation//gmd:LocalisedCharacterString[@locale='#fra']/text()" />
+          </p>
+        </xsl:if>
+      </xsl:comment>
+
+      <xsl:if test="//gmd:pointOfContact//gmd:individualName/* != '' 
+              or //gmd:pointOfContact//gmd:organisationName//gmd:LocalisedCharacterString[@locale='#fra']/text() != ''
+              or //gmd:pointOfContact//gmd:positionName//gmd:LocalisedCharacterString[@locale='#fra']/text() != ''
+              or //gmd:pointOfContact//gmd:electronicMailAddress/* != ''
+              or //gmd:pointOfContact//gmd:role/gmd:CI_RoleCode/@codeListValue != ''">
+        <h5 class="text-xl font-bold mb-3">{{metadata.xslt.contactInfo}}</h5>
+        <p>
+          <xsl:value-of select="//gmd:pointOfContact//gmd:individualName" />
+        </p>
+        <p>
+          <xsl:value-of select="//gmd:pointOfContact//gmd:organisationName//gmd:LocalisedCharacterString[@locale='#fra']/text()" />
+        </p>
+        <p>
+          <xsl:value-of select="//gmd:pointOfContact//gmd:positionName//gmd:LocalisedCharacterString[@locale='#fra']/text()" />
+        </p>
+        <p>
+          <a href="mailto:{//gmd:pointOfContact//gmd:electronicMailAddress//gmd:LocalisedCharacterString[@locale='#fra']/text()}?Subject={//gmd:identificationInfo//gmd:title//gmd:LocalisedCharacterString[@locale='#fra']/text()}">
+            <xsl:value-of select="//gmd:pointOfContact//gmd:electronicMailAddress" />
+          </a>
+        </p>
+        <p>
+          <xsl:variable name="roleCode" >
+            <xsl:value-of select="concat(substring(//gmd:pointOfContact//gmd:role/gmd:CI_RoleCode/@codeListValue,1,1),
+                        substring(//gmd:pointOfContact//gmd:role/gmd:CI_RoleCode/@codeListValue, 2))" />
+          </xsl:variable>
+
+          <xsl:choose>
+            <xsl:when test="$roleCode = 'resourceProvider'">{{metadata.xslt.resourceProvider}}</xsl:when>
+            <xsl:when test="$roleCode = 'custodian'">{{metadata.xslt.custodian}}</xsl:when>
+            <xsl:when test="$roleCode = 'owner'">{{metadata.xslt.owner}}</xsl:when>
+            <xsl:when test="$roleCode = 'user'">{{metadata.xslt.user}}</xsl:when>
+            <xsl:when test="$roleCode = 'distributor'">{{metadata.xslt.distributor}}</xsl:when>
+            <xsl:when test="$roleCode = 'originator'">{{metadata.xslt.originator}}</xsl:when>
+            <xsl:when test="$roleCode = 'pointOfContact'">{{metadata.xslt.pointOfContact}}</xsl:when>
+            <xsl:when test="$roleCode = 'principalInvestigator'">{{metadata.xslt.principalInvestigator}}</xsl:when>
+            <xsl:when test="$roleCode = 'processor'">{{metadata.xslt.processor}}</xsl:when>
+            <xsl:when test="$roleCode = 'publisher'">{{metadata.xslt.publisher}}</xsl:when>
+            <xsl:when test="$roleCode = 'author'">{{metadata.xslt.author}}</xsl:when>
+            <xsl:when test="$roleCode = 'collaborator'">{{metadata.xslt.collaborator}}</xsl:when>
+            <xsl:when test="$roleCode = 'editor'">{{metadata.xslt.editor}}</xsl:when>
+            <xsl:when test="$roleCode = 'mediator'">{{metadata.xslt.mediator}}</xsl:when>
+            <xsl:when test="$roleCode = 'rightsHolder'">{{metadata.xslt.rightsHolder}}</xsl:when>
+          </xsl:choose>
+        </p>
+      </xsl:if>
+
+      <xsl:if test="$catalogue_url != ''">
+        <h5 class="text-xl font-bold mb-3">{{metadata.xslt.cataloguePage}}</h5>
+        <p>
+          <a href="{$catalogue_url}"
+             rel="external" target="_blank" class="ui-link">
+            {{metadata.xslt.metadata}}
+          </a>
+        </p>
+      </xsl:if>
+    </div>
+  </xsl:template>
+</xsl:stylesheet>
+`,K={key:0},W={class:"flex justify-center"},Z={key:0,class:"flex flex-col justify-center text-center"},J=["innerHTML"],Q=["innerHTML"],ee={key:3,class:"flex flex-col justify-center text-center"},te={class:"text-xl mt-20"},ae={key:1,class:"p-5"},ne=E({__name:"screen",props:{panel:{type:Object,required:!0},payload:{type:Object,required:!0}},setup(r){const s=A(),{t:o}=R(),f=V("iApi"),I=T(),l=r,u=_(()=>s.status),v=_(()=>s.response),b=T(!1),x=C({}),y=C([]),$=C([]);j(()=>{S(),y.push(f.event.on(X.LAYER_REMOVE,e=>{l.payload.layer?.uid===e.uid&&l.panel.close()})),$.push(q(()=>l.payload.layer.uid,(e,t)=>{e!==t&&S()}))}),F(()=>{y.forEach(e=>f.event.off(e)),$.forEach(e=>e())});const S=()=>{b.value=l.payload.layer!==void 0&&!l.payload.layer.isRemoved,l.payload.type==="xml"?M(l.payload.url,[]).then(e=>{if(s.status="success",e!==null){const t=document.createElement("div");t.appendChild(g(`${e.firstElementChild.outerHTML}`)),(l.payload.catalogueUrl||l.payload.url)&&t.appendChild(g(`<h5 class="text-xl font-bold mb-3">${o("metadata.xslt.metadata")}</h5>`)),l.payload.catalogueUrl&&t.appendChild(g(`<p><a style="color: blue;" href="${l.payload.catalogueUrl}" target="_blank">${o("metadata.xslt.cataloguePage")}</a></p>`)),t.appendChild(g(`<p><a style="color: blue;" href="${l.payload.url}" target="_blank">${o("metadata.xslt.metadataPage")}</a> (xml)</p>`)),s.response=t.outerHTML}}):l.payload.type==="html"?w(l.payload.url).then(e=>{s.status=e.status,s.response=e.response}):l.payload.type==="md"&&w(l.payload.url).then(e=>{s.status=e.status,s.response=D(e.response)})},M=(e,t)=>{let a=f.language==="en"?Y:z;return a=a.replace(/\{\{([\w.]+)\}\}/g,(i,m)=>o(m)),x[e]?Promise.resolve(L(x[e],a,t)):w(e).then(i=>(x[e]=i.response,L(x[e],a,t)))},L=(e,t,a)=>{let i=null;if(window.XSLTProcessor){const m=new window.XSLTProcessor,P=new DOMParser,N=P.parseFromString(e,"text/xml"),H=P.parseFromString(t,"text/xml");m.importStylesheet(H),a&&a.forEach(O=>m.setParameter("",O.key,O.value||"")),i=m.transformToFragment(N,document)}return i},w=e=>new Promise(t=>{const a=new XMLHttpRequest;a.open("GET",e,!0),a.responseType="text",a.onload=()=>{a.status===200?t({status:"success",response:a.response}):t({status:"error",response:"Could not load results from remote service."})},a.onerror=()=>{t({status:"error",response:"Could not load results from remote service."})},a.send()});function g(e){const t=document.createElement("div");return t.innerHTML=e,t}return(e,t)=>{const a=U("panel-screen");return n(),G(a,{panel:r.panel,ref_key:"el",ref:I},{header:k(()=>[B(c(p(o)("metadata.title"))+": "+c(r.payload.layerName),1)]),content:k(()=>[b.value?(n(),d("div",K,[h("div",W,[u.value=="loading"?(n(),d("div",Z,c(p(o)("metadata.loading")),1)):r.payload.type==="xml"&&u.value=="success"?(n(),d("div",{key:1,innerHTML:v.value,class:"flex flex-col justify-center xml-content"},null,8,J)):(r.payload.type==="html"||r.payload.type==="md")&&u.value=="success"?(n(),d("div",{key:2,innerHTML:v.value,class:"flex flex-col justify-center max-w-full metadata-view"},null,8,Q)):(n(),d("div",ee,[t[0]||(t[0]=h("img",{src:"https://i.imgur.com/fA5EqV6.png"},null,-1)),h("span",te,c(p(o)("metadata.error")),1)]))])])):(n(),d("div",ae,[h("span",null,c(p(o)("metadata.label.no.layer")),1)]))]),_:1},8,["panel"])}}});export{ne as default};
