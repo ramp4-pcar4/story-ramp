7add1891a7cc9f579e3283b47d965ee75d76e9b0
Yi Lei Feng <fengyilei1@gmail.com>
2025-05-22 18:24:33 +0000

    Delete PR demo from GH pages issue-553

--- issue-553/assets/panel-Ci2ek2q4.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./story-C8cpA3MP.js";import"./main-CgC6dfHJ.js";export{m as default};
--- issue-553/assets/vue.esm-bundler-D2erd_gM-k-TXfQso.js
+++
@@ -1,1 +0,0 @@
-import{fA as o,gw as t}from"./story-C8cpA3MP.js";const r=o(t);export{r as o};
